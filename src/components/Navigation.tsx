7021e1728d921d77acdd5df49f584751d2635fec
LaupWing <loc_strict@hotmail.com>
2022-12-14 17:16:31 +0100

    udpate

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,13 +1,31 @@
-import { AppBar, Toolbar, Typography } from "@mui/material"
+import { AppBar, Toolbar, Typography, Link } from "@mui/material"
 import React from 'react'
 
 const Navigation = () => {
    return (
       <AppBar position="static">
          <Toolbar>
-            <Typography mr={2}>All products</Typography>
-            <Typography mr={2}>Women</Typography>
-            <Typography>Men</Typography>
+            <Link 
+               href="/"
+               underline="none"
+               color={"inherit"}
+            >
+               <Typography mr={2}>All products</Typography>
+            </Link>
+            <Link 
+               href="/collections/women"
+               underline="none"
+               color={"inherit"}
+            >
+               <Typography mr={2}>Women</Typography>
+            </Link>
+            <Link 
+               href="/collections/men"
+               underline="none"
+               color={"inherit"}
+            >
+               <Typography>Men</Typography>
+            </Link>
          </Toolbar>
       </AppBar>
    )
