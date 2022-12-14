f98f2c9d005ef2c14fc0871750ddee7860c559ab
LaupWing <loc_strict@hotmail.com>
2022-12-14 17:10:26 +0100

    udpate

---
+++ src/pages/collections/[collectionName].tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Container, Box} from "@mui/material"
+import Navigation from "../../components/Navigation"
+import BreadcrumbsNavigation from "../../components/Breadcrumbs"
+import { useRouter } from "next/router"
+
+const CollectionPage = () => {
+   const router = useRouter()
+   
+   const {collectionName} = router.query
+
+   return (
+      <Box>
+         <Navigation />
+         <Container maxWidth="lg">
+            <BreadcrumbsNavigation title={collectionName as string}/>
+         </Container>
+      </Box>
+   )
+}
+
+export default CollectionPage
