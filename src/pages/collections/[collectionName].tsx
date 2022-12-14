6acd16310a64c6da48d1116a710ab75e97fe1b68
LaupWing <loc_strict@hotmail.com>
2022-12-14 17:12:56 +0100

    update

--- src/pages/collections/[collectionName].tsx
+++ src/pages/collections/[collectionName].tsx
@@ -3,17 +3,21 @@ import {Container, Box} from "@mui/material"
 import Navigation from "../../components/Navigation"
 import BreadcrumbsNavigation from "../../components/BreadcrumbsNavigation"
 import { useRouter } from "next/router"
+import PRODUCTS from "../../data"
+import ProductList from "../../components/ProductList"
 
 const CollectionPage = () => {
    const router = useRouter()
    
    const {collectionName} = router.query
+   const products = PRODUCTS.filter(product => product.collection === collectionName)
 
    return (
       <Box>
          <Navigation />
          <Container maxWidth="lg">
             <BreadcrumbsNavigation title={collectionName as string}/>
+            <ProductList products={products}/>
          </Container>
       </Box>
    )
