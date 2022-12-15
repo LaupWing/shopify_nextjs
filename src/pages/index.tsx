70e9880cb9aea96a0f1b2a0e6b0a2e0963e088af
LaupWing <loc_strict@hotmail.com>
2022-12-15 12:53:24 +0100

    producttts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { Box, Container } from "@mui/material"
+import { parseShopifyResponse, shopifyCLient } from "../../lib/shopify"
 import Navigation from "../components/Navigation"
 import ProductList from "../components/ProductList"
 import PRODUCTS from "../data"
@@ -12,4 +13,14 @@ export default function Home() {
          </Container>
       </Box>
    )
+}
+
+export const getServerSideProps = async () =>{
+   const products = await shopifyCLient.product.fetchAll()
+
+   return {
+      props:{ 
+         products: parseShopifyResponse(products)
+      }
+   }
 }
